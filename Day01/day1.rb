be5edd0d11cc2438eab4c9121eaa5963f6b14890
Nancy Tsatsara <ntsatsara@skroutz.gr>
2021-12-11 11:56:52 +0200

    Day 1 part 2

--- Day01/day1.rb
+++ Day01/day1.rb
@@ -1,10 +1,24 @@
-file = File.open("input.txt")
+# frozen_string_literal: true
+
+def part1(data)
+  count = 0
+  (1...data.size).each do |index|
+    count += 1 if data[index] > data[index - 1]
+  end
+
+  puts count
+end
+
+def part2(data)
+  windows = []
+  (0...data.size - 2).each do |_index|
+    windows.push(data[_index] + data[_index + 1] + data[_index + 2])
+  end
+  part1(windows)
+end
+
+file = File.open('input.txt')
 file_data = file.readlines.map(&:chomp)
 file_data.map!(&:to_i)
 
-count = 0
-(1...file_data.size).each do |index|
-  count += 1 if file_data[index] > file_data[index - 1]
-end
-
-puts count
+part2(file_data)
